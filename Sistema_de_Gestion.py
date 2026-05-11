8f7fdb1d8c422d2f78642eb5c87a642e1b5dc39d
Leris Yuranny <lerisyuranny121@gmail.com>
2026-05-11 01:19:01 -0500

    Cambio general a tener en cuenta

--- Sistema_de_Gestion.py
+++ Sistema_de_Gestion.py
@@ -1,18 +1,43 @@
-#---------------CLASE CLIENTE--------------
-
-# Se importan las clases necesarias desde el módulo core y abc.
-from core import ValidationError, LogSystem
-from core import EntidadSistema
+### IMPORTACIONES ###
 from abc import ABC, abstractmethod
+from datetime import datetime
 
+### EXCEPCIONES PERSONALIZADAS ###
+class ValidationError(Exception):
+    """Excepción personalizada para errores de validación."""
+    pass
+
+class ReservationError(Exception):
+    """Excepción personalizada para errores relacionados con reservas."""
+    pass
+
+class ServiciosNoDisponiblesError(Exception):
+    """Excepción personalizada para errores relacionados con servicios."""
+    pass
+
+### SISTEMA LOGS ###
+class LogSystem:
+    def write(self, message):
+        with open("sistema_logs.txt", "a", encoding="utf-8") as archivo:
+            fecha = datetime.now()
+            archivo.write(f"[{fecha}] {message}\n")
+
+### CLASE ABSTRACTA GENERAL ###
+class EntidadSistema(ABC):
+    @abstractmethod
+    def get_info(self):
+        pass
+
+
+#---------------CLASE CLIENTE--------------
 class Cliente(EntidadSistema):
-    
-    "Clase que representa a un cliente en el sistema de gestión," 
-    "con atributos privados para nombre, email y teléfono."
+    """Clase que representa a un cliente en el sistema de gestión,
+    con atributos privados para nombre, email y teléfono."""
     
     def __init__(self, nombre, email, telefono):
         
-        "Constructor de la clase Cliente, que inicializa los atributos y llama al método de validación."
+        """Constructor de la clase Cliente, que inicializa los atributos 
+        y llama al método de validación."""
         
         # Se asignan los valores a los atributos privados.
         self.__nombre = nombre
@@ -22,12 +47,14 @@ class Cliente(EntidadSistema):
         # Se llama al método de validación para verificar los datos del cliente.
         self.validar()
     
-    
+    # Validar datos
     def validar(self):
         
-        "Método que valida los datos del cliente."
-        "Lanza una excepción de validación si los datos no son correctos"
-        "y registra el error en el sistema de logs."
+        """Método que valida los datos del cliente.
+
+        Lanza una excepción de validación si los datos no son correctos
+        y registra el error en el sistema de logs.
+        """
     
         # Se crea una instancia del sistema de logs.
         logger = LogSystem()
@@ -36,62 +63,77 @@ class Cliente(EntidadSistema):
             # Se verifica que el nombre no esté vacío.
             if not self.__nombre:
                 # Si el nombre está vacío, se lanza una excepción de validación.
-                raise ValidationError("El nombre del cliente no puede estar vacío.")
+                raise ValidationError(
+                    "El nombre del cliente no puede estar vacío."
+                    )
             
             # Se verifica que el email contenga un "@" para ser considerado válido.
             if not self.__email or "@" not in self.__email:
                 # Si el email no es válido, se lanza una excepción de validación.
-                raise ValidationError("El email del cliente no es válido.")
+                raise ValidationError(
+                    "El email del cliente no es válido."
+                    )
             
             # Se verifica que el teléfono contenga solo dígitos.
             if not self.__telefono or not self.__telefono.isdigit():
                 # Si el teléfono no es válido, se lanza una excepción de validación.
-                raise ValidationError("El teléfono del cliente debe contener solo números.")
+                raise ValidationError(
+                    "El teléfono del cliente debe contener solo números."
+                    )
         
         # Se captura la excepción de validación.    
         except ValidationError as e:
             
             # Se registra el error de validación en el sistema de logs.
-            logger.write(f"Error de validación: {e}")
+            logger.write(
+                f"Error de validación: {e}"
+                )
             
             # Se vuelve a lanzar la excepción para que el sistema de gestión la maneje.
             raise
     
+    # Información del cliente
     def get_info(self):
         
-        "Método que devuelve la información del cliente en formato de cadena."
+        """Método que devuelve la información del 
+        cliente en formato de cadena."""
         
-        return f"Cliente: {self.__nombre}, Email: {self.__email}, Teléfono: {self.__telefono}"
+        return (
+            f"Cliente: {self.__nombre}, "
+            f"Email: {self.__email},"
+            f"Teléfono: {self.__telefono}"
+            )
     
+    # Encapsulación
     @property
-    def nombre(self):
-        
-        "Propiedad que devuelve el nombre del cliente."
-        
+    def nombre(self): 
+        """Propiedad que devuelve el nombre del cliente."""
         return self.__nombre
         
     @property
     def email(self):
-
-       "Propiedad que devuelve el email del cliente."
-
+       """Propiedad que devuelve el email del cliente."""
        return self.__email
 
     @property
     def telefono(self):
-
-        "Propiedad que devuelve el telefono del cliente."
-
+        """Propiedad que devuelve el telefono del cliente."""
         return self.__telefono
-        
+    
+
+       
 #---------------CLASE SERVICIO --------------
 class Servicio(ABC):
 
-# Clase servicio para calcular costos, describir servicios y validar parámetros, ademas de implementar tres servicios.
+    """Clase servicio para calcular costos, describir servicios 
+    y validar parámetros, ademas de implementar tres servicios."""
     
     def __init__(self, nombre_servicio, precio_base):
         self.nombre_servicio = nombre_servicio
         self.precio_base = precio_base
+        
+        if precio_base < 0:
+            raise ValidationError("El precio base no puede ser negativo.")
 
     # Se obliga a implementar el calculo de costos con sobrecarga.
     @abstractmethod
@@ -102,22 +144,33 @@ class Servicio(ABC):
     @abstractmethod
     def describir_servicios(self):
         pass
-
+    
     # Metodo para validar que el valor o los datos no sean negativos.
-    def validar_parametros(self, valor):
-        if valor < 0:
+    def validar_parametros(self, valor, nombre_parametro) -> bool:
+        if valor <= 0:
             # Si el valor es negativo, lanza un mensaje de error.
-            raise ValidationError("Error en {self.nombre_servicio} el valor no puede ser negativo.")
+            raise ValidationError(
+                f"{self.nombre_servicio} el valor de {nombre_parametro} no puede ser negativo."
+                )
         return True
 
-# Se crea la clase reserva de salas y se definen los servicios incluidos.
-class reservas_de_salas(Servicio):
+# SE CREA LA CLASE RESERVA DE SALAS Y SE DEFINEN LOS SERVICIOS INCLUIDOS.
+class ReservaSala(Servicio):
     def describir_servicios(self):
-        return "Reserva de sala: {self.nombre_servicio} con todos los servicios incluidos."
+        return(
+            f"Reserva de sala: {self.nombre_servicio}. con todos los servicios incluidos."
+        )
 
     # Se definen las horas y la inclusion de la categoria.
-    def calcular_costos(self, horas=1, incluye_categoria=False):
-        self.validar_parametros(horas, "horas")
+    @abstractmethod
+    def calcular_costos(
+        self, 
+        horas=1,
+        incluye_categoria=False
+    ):
+        
+        self.validar_parametros(horas, "Horas")
+        
         # Logica: precio x hora + recargo opcional por categoria.
         total = self.precio_base * horas
         if incluye_categoria:
@@ -125,14 +178,19 @@ class reservas_de_salas(Servicio):
             total += 250000
         return total
         
-# Se crea la clase alquiler de equipos y se incluye el servicio tecnico.
-class alquiler_de_equipos(Servicio):
+# SE CREA LA CLASE ALQUILER DE EQUIPOS Y SE INCLUYE EL SERVICIO TECNICO.
+class AlquilerEquipo(Servicio):
     def describir_servicios(self):
-        return "Alquiler de equipo: {self.nombre_servicio}. Ademas incluye servicio tecnico basico"
+        return (
+            f"Alquiler de equipo:"
+            f"{self.nombre_servicio}. Ademas incluye servicio tecnico basico"
+        )
 
     # Se definen los dias y el seguro opcional.
-    def calcular_costos(self, dias=1, seguro_opcional=True):
-        self.validar_parametros(dias, "dias")
+    def calcular_costos(
+        self, dias=1, seguro_opcional=True
+        ):
+        self.validar_parametros(dias, "Dias")
         # Logica: precio por dia con el descuento por mas de (5 dias).
         total = self.precio_base * dias
         if dias > 5:
@@ -140,26 +198,32 @@ class alquiler_de_equipos(Servicio):
             total *= 0.80
         if seguro_opcional:
             # 10% del seguro por dia.
-            total += (self.precio_base 0.10) * dias
+            total += (self.precio_base * 0.10) * dias
             return total
 
-# Se crea la clase asesorias especializadas 
-class asesorias_especializadas(Servicio):
+# SE CREA LA CLASE ASESORIAS ESPECIALIZADAS.
+class AsesoriaEspecializada(Servicio):
     def describir_servicios(self):
-        return "Asesoria Especializada: {self.nombre_servicio}. Consultoria tecnica con los mejores expertos"
+        return(
+            f"Asesoria Especializada:"
+            f"{self.nombre_servicio}. Consultoria tecnica con los mejores expertos"
+        )
 
     # Se definen las horas y la urgencia.
-    def calcular_costos(self, horas=1, es_de_urgencia=False):
-        self.validar_paramteros(horas, "horas")
+    def calcular_costos(
+        self, horas=1, es_de_urgencia=False
+        ):
+        self.validar_parametros(horas, "Horas")
         # Logica: precio por hora con el recargo por la urgencia.
         tarifa_final = self.precio_base
         if es_de_urgencia:
             # 50% de racargo por urgencia
             tarifa_final *= 1.50
             return tarifa_final * horas
- 
-   #---------------CLASE RESERVA--------------
 
+
+ 
+#---------------CLASE RESERVA--------------
 class Reserva:
 
     """
@@ -202,7 +266,9 @@ class Reserva:
                 f"Error al crear reserva: {e}"
             )
 
-            raise
+            raise ReservationError(
+                "No se puede crear la reserva"
+            ) from e
 
     # MÉTODO PARA CONFIRMAR RESERVA
     def confirmar(self):
@@ -228,7 +294,9 @@ class Reserva:
                 f"Error al confirmar reserva: {e}"
             )
 
-            raise
+            raise ReservationError(
+                "No se pudo crear la reserva"
+            ) from e
 
     # MÉTODO PARA CANCELAR RESERVA
     def cancelar(self):
@@ -308,12 +376,12 @@ try:
         "carlos@gmail.com",
         "3001234567"
     )
-
-    servicio1 = reservas_de_salas(
+    
+    servicio1 = ReservaSala(
         "Sala VIP",
         100000
     )
-
+    
     reserva1 = Reserva(
         cliente1,
         servicio1,
@@ -337,11 +405,24 @@ try:
 except Exception as e:
 
     print("Error:", e) 
+
+
        
 #---------------MÉTODOS SOBRECARGADOS--------------
-    def calcular_costo_final(self, precio_base, impuesto=0, descuento=0, cargo_extra=0):
+class CalculadoraCostos:
+    
+    """Clase que implementa métodos para calcular costos de servicios,
+    aplicando descuentos e impuestos según sea necesario."""
+    def calcular_costo_final(
+        self, 
+        precio_base,
+        impuesto=0,
+        descuento=0,
+        cargo_extra=0
+        ):
         
-        "Método que calcula el costo final de un servicio, aplicando un descuento si es necesario."
+        """Método que calcula el costo final de un servicio,
+        aplicando un descuento si es necesario."""
         
         # Se crea una instancia del sistema de logs.
         logger = LogSystem()
@@ -398,12 +479,16 @@ except Exception as e:
             logger.write(f"Error de validación: {e}")
             # Se vuelve a lanzar la excepción para que el sistema de gestión la maneje.
             raise
+        
+        
+        
+#-------------10 OPERACIONES DEL SISTEMA-------------
 print("\n===== 10 OPERACIONES DEL SISTEMA =====")
 
 
 # OPERACIÓN 1
 try:
-    cliente1 = Cliente("Ana", "ana@gmail.com")
+    cliente1 = Cliente("Ana", "ana@gmail.com", "3009876543")
     print("1. Cliente registrado correctamente")
 
 except Exception as e:
@@ -412,10 +497,10 @@ except Exception as e:
 
 # OPERACIÓN 2
 try:
-    cliente2 = Cliente("", "correo")
+    cliente2 = Cliente("", "correo", "abc")
     print("2. Cliente registrado")
 
-except Exception as e:
+except ValidationError as e:
     print("2. Error controlado:", e)
 
 
@@ -423,14 +508,14 @@ except Exception as e:
 try:
     servicio1 = ReservaSala("Sala VIP", 100000)
     print("3. Servicio creado correctamente")
-
+    
 except Exception as e:
     print("3. Error:", e)
 
 
 # OPERACIÓN 4
 try:
-    servicio2 = Asesoria("Marketing", -500)
+    servicio2 = AsesoriaEspecializada("Marketing", -500)
     print("4. Servicio creado")
 
 except Exception as e:
@@ -476,8 +561,8 @@ except Exception as e:
 # OPERACIÓN 9
 try:
     servicio3 = AlquilerEquipo("VideoBeam", 80000)
-
-    total = servicio3.calcular_costo()
+    
+    total = servicio3.calcular_costos()
 
     print("9. Costo calculado:", total)
 
